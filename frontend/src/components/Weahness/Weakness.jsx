4d111f3612964f69f62110ef183e930c4178eea2
WAQUET Valentin <waquetv@gmail.com>
2022-11-08 13:17:23 +0100

    Add style for header card and weakness

--- frontend/src/components/Weahness/Weakness.jsx
+++ frontend/src/components/Weahness/Weakness.jsx
@@ -1,8 +1,11 @@
+import image from "@assets/fire.png";
+import Style from "./Style";
+
 export default function Weakness() {
   return (
-    <>
+    <Style>
       <p>Weakness:</p>
-      <p>Fire</p>
-    </>
+      <img src={image} alt="" />
+    </Style>
   );
 }
