0f33b62619ca6c79a42176236b94b1855cb23a5c
WAQUET Valentin <waquetv@gmail.com>
2022-11-17 10:56:53 +0100

    Add button on Home page

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <button type="button">Deck</button>
+      <button type="button">Play</button>
+      <button type="button">Collection</button>
+      <button type="button">Contact</button>
+    </>
+  );
+}
---
+++ frontend/src/pages/Home/style.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import image from "@assets/background.png";
+
+export default styled.section`
+  background-image: url(${image});
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
