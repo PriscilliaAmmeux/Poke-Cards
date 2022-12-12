803665ae8015e3923d7d1ae39eb589a1066f7451
Sax59 <waquetv@gmail.com>
2022-12-12 22:23:54 +0100

    Stylised home page and add image

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,24 +1,28 @@
 import { Link } from "react-router-dom";
-import background from "@assets/background.jpg";
+import pokeball from "@assets/pokeball.png";
 import Style from "./style";
 
 export default function Home() {
   return (
     <Style>
-      <img src={background} alt="background home" />
       <ul>
-        <Link to="/deck">
-          <li>My Deck</li>
-        </Link>
-        <Link to="/collection">
-          <li>Collection</li>
-        </Link>
-        <Link to="/gamesPages">
-          <li>Play</li>
-        </Link>
-        <Link to="/contact">
-          <li>Contact</li>
-        </Link>
+        <div className="right">
+          <Link className="linkbtn" to="/deck">
+            <li>My Deck</li>
+          </Link>
+          <Link className="linkbtn" to="/collection">
+            <li>Collection</li>
+          </Link>
+        </div>
+        <img className="pokeball" src={pokeball} alt="Pokeball" />
+        <div className="left">
+          <Link className="linkbtn" to="/gamesPages">
+            <li>Play</li>
+          </Link>
+          <Link className="linkbtn" to="/contact">
+            <li>Contact</li>
+          </Link>
+        </div>
       </ul>
     </Style>
   );
