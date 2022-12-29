05554518b21607f7ec8400e881418da6f8bfee07
Sax59 <waquetv@gmail.com>
2022-12-29 17:27:56 +0100

    Creat function for count amount in user wallet

--- frontend/src/components/PledgeCount/PledgeCount.jsx
+++ frontend/src/components/PledgeCount/PledgeCount.jsx
@@ -1,16 +1,37 @@
+import PropTypes from "prop-types";
 import Style from "./style";
 
-export default function PledgeCount() {
+export default function PledgeCount({
+  user,
+  purchaseValue,
+  wallet,
+  setWallet,
+}) {
+  const handleClick = () => {
+    setWallet(user - purchaseValue);
+  };
   return (
     <Style>
       <h2>Your Count</h2>
       <p>
-        Your actual wallet: <span>xxxxxxxxx$</span>
+        Your actual wallet: <span>{user}$</span>
       </p>
       <p>
-        Count: <span>xxxxxxxx$</span>
+        Purchase amount: <span>{purchaseValue}$</span>
       </p>
-      <button type="button">Buy</button>
+      <p>
+        Count: <span>{wallet}$</span>
+      </p>
+      <button type="button" onClick={handleClick}>
+        Buy
+      </button>
     </Style>
   );
 }
+
+PledgeCount.propTypes = {
+  user: PropTypes.number.isRequired,
+  purchaseValue: PropTypes.number.isRequired,
+  wallet: PropTypes.number.isRequired,
+  setWallet: PropTypes.number.isRequired,
+};
