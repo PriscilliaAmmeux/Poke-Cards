6656d5b62407149df324ca8fe58efacb697f0574
Priscillia <selvapriscillia@yahoo.fr>
2023-03-09 11:13:43 +0100

    link form to emailjs + add pop up sweet alert

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -1,23 +1,52 @@
 import NavBar from "@components/Navbar/NavBar";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
 import Style from "./style";
 
 export default function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      "service_rvb51jb",
+      "template_ehbe1bk",
+      form.current,
+      "SZ3K--33UQN71Zi6Z"
+    );
+    e.target.reset();
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Message send",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
   const [isActive] = useState("Contact");
   return (
     <Style>
       <NavBar isActive={isActive} />
       <div className="form">
         <h1 className="titleForm">Contact Us</h1>
-        <label htmlFor="email">
-          Email:
-          <input type="email" name="email" id="email" />
-        </label>
-        <label htmlFor="message">
-          Your request:
-          <textarea name="message" id="message" cols="30" rows="10" />
-        </label>
-        <input className="submit" type="submit" value="Submit" />
+        <form ref={form} onSubmit={sendEmail} className="form">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </Style>
   );
